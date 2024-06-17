0bbc5d66fda8c14b1c0975afd558625105be2e17
Dhvanishere <dhvanishere@gmail.com>
2024-06-17 12:42:39 -0400

    svm.py file is pulled from cristeena

--- svm.py
+++ svm.py
@@ -1,10 +1,5 @@
-from svm_preprocessing import load_data_folder,train_test_process
+from svm_preprocessing import load_data_folder
 from data_utils import load_CIFAR10
-import numpy as np
 
 load_data_folder()
-root = './data/CIFAR-10/cifar-10-batches-py'
-X_train,y_train,X_test,y_test = load_CIFAR10(root)
-X_dev,y_dev,X_val,y_val = train_test_process(X_train,y_train,X_test,y_test)
-
-
+X_train,X_test,y_train,y_test = load_CIFAR10()
